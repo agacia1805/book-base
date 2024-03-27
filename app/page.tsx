356aabe648605eb3b188a9fdffe5794bd85b9cf3
agacia1805 <agacia1805@gmail.com>
2024-03-27 23:39:00 +0100

    (chore) run prettier fixes

--- app/page.tsx
+++ app/page.tsx
@@ -6,25 +6,24 @@ export default function Page() {
     <main className='flex flex-col p-6'>
       <div className='mt-4 flex grow flex-col gap-4 md:flex-row'>
         <div className='flex flex-col justify-center gap-6 rounded-lg px-6 py-10 md:w-3/5'>
-          <p className="text-xl text-gray-300 md:text-3xl md:leading-normal">
+          <p className='text-xl text-gray-300 md:text-3xl md:leading-normal'>
             <strong>Welcome to BookBase.</strong>
-            <span className="block">Store your favourite books
-            today. </span>
+            <span className='block'>Store your favourite books today. </span>
           </p>
-          <div className="flex gap-4">
-          <Link
-            href='/login'
-            className='flex items-center gap-5 self-start rounded-lg px-6 py-3 text-sm font-medium text-white transition-colors md:text-base'
-          >
-            <span>Log in</span>
-          </Link>
+          <div className='flex gap-4'>
             <Link
-                      href='/signup'
-                      className='flex items-center gap-5 self-start rounded-lg px-6 py-3 text-sm font-medium text-white transition-colors md:text-base'
-                    >
-                      <span>Sign up</span>
-                    </Link>
-                    </div>
+              href='/login'
+              className='flex items-center gap-5 self-start rounded-lg px-6 py-3 text-sm font-medium text-white transition-colors md:text-base'
+            >
+              <span>Log in</span>
+            </Link>
+            <Link
+              href='/signup'
+              className='flex items-center gap-5 self-start rounded-lg px-6 py-3 text-sm font-medium text-white transition-colors md:text-base'
+            >
+              <span>Sign up</span>
+            </Link>
+          </div>
         </div>
         <Image
           src='/image.svg'
--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -8,9 +8,7 @@ export default function LoginForm() {
   return (
     <form className='space-y-3'>
       <div className='flex-1 rounded-lg bg-gray-50 px-6 pb-4 pt-8'>
-        <h1 className="mb-3 text-2xl">
-          Please log in to continue.
-        </h1>
+        <h1 className='mb-3 text-2xl'>Please log in to continue.</h1>
         <div className='w-full'>
           <div>
             <label
@@ -51,15 +49,15 @@ export default function LoginForm() {
           </div>
         </div>
         <LoginButton />
-               <div
-                 className="flex h-8 items-end space-x-1"
-                 aria-live="polite"
-                 aria-atomic="true"
-               >
-                   <>
-                     <p className="text-sm text-red-500">error message</p>
-                   </>
-               </div>
+        <div
+          className='flex h-8 items-end space-x-1'
+          aria-live='polite'
+          aria-atomic='true'
+        >
+          <>
+            <p className='text-sm text-red-500'>error message</p>
+          </>
+        </div>
       </div>
     </form>
   );
@@ -68,9 +66,5 @@ export default function LoginForm() {
 function LoginButton() {
   const { pending } = useFormStatus();
 
-  return (
-    <Button className="mt-4 w-full">
-      Log in
-    </Button>
-  );
+  return <Button className='mt-4 w-full'>Log in</Button>;
 }
