57dec165d1d73eff181bbdef34718964b9707b8a
agacia1805 <agacia1805@gmail.com>
2024-05-13 23:05:18 +0100

    (chore) update next config for vercel blob storage

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'book-base.public.blob.vercel-storage.com',
+        port: '',
+      },
+    ],
+  },
+};
 export default nextConfig;
