f771b7ed889e95ebeb160c763f4e2831fb0e94fd
agacia1805 <agacia1805@gmail.com>
2024-05-16 16:12:12 +0100

    (chore) update types and styling

--- app/lib/definitions.ts
+++ app/lib/definitions.ts
@@ -8,3 +8,14 @@ export type Book = {
   rating: number;
   genre: string;
 };
+
+export interface IBookCardProps {
+  id: string;
+  title: string;
+  author: string;
+  description: string;
+  image: string;
+  status: string;
+  rating: number;
+  genre: string;
+}
