9ead53a53b0abd779c5742dbc609effdcf80437c
agacia1805 <agacia1805@gmail.com>
2024-05-31 20:29:53 +0100

    (chore) update styling

--- app/ui/button.tsx
+++ app/ui/button.tsx
@@ -10,8 +10,8 @@ export function Button({ children, className, ...rest }: ButtonProps) {
     <button
       {...rest}
       className={clsx(
-        'flex h-10 items-center rounded-lg bg-gray-100 px-4 text-base font-medium text-[#081a57] transition-opacity hover:opacity-80 focus-visible:outline focus-visible:outline-1 focus-visible:outline-offset-1 focus-visible:outline-gray-400 aria-disabled:cursor-not-allowed aria-disabled:opacity-50',
-        className
+        className,
+        'flex h-10 items-center rounded-lg px-4 text-base font-medium text-[#081a57] transition-opacity hover:opacity-80 focus-visible:outline focus-visible:outline-1 focus-visible:outline-offset-1 focus-visible:outline-gray-400 aria-disabled:cursor-not-allowed aria-disabled:opacity-50'
       )}
     >
       {children}
